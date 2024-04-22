6dc227db5f950121aa09ebec8614cb6fb047108c
applepie744 <parr.anna74@mail.ru>
2024-04-22 23:09:37 +0300

    fix

--- src/app/entities/components/data-table/data-sourse.ts
+++ src/app/entities/components/data-table/data-sourse.ts
@@ -10,4 +10,5 @@ export const CHARACTER_DATA: сharacter[] = [
     { id: 1, name: 'Hero', power: 4, skills: ['Прыгучесть'], level: 10 },
     { id: 2, name: 'Alis', power: 1, skills: ['Регенерация','Магия'], level: 6 },
     { id: 3, name: 'Alex', power: 2, skills: ['Мантия','Сила'], level: 3 },
+    { id: 3, name: 'Alex', power: 2, skills: ['Мантия','Сила'], level: 3 },
 ];
